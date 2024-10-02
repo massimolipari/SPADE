0c7816b1829731ef3297b692fec3034f0d7900ab
Massimo Lipari <massimo.lipari@mail.mcgill.ca>
2024-10-02 17:01:38 +0100

    Update import.py

--- import.py
+++ import.py
@@ -1,5 +1,6 @@
 from polyglotdb import CorpusContext
 import polyglotdb.io as pgio
+from polyglotdb.query.base.func import Count, Average
 
 corpus_root = '/home/mlipari/spade-SOTC/audio_and_transcripts'
 corpus_name = 'spade-SOTC'
@@ -9,7 +10,18 @@ parser = pgio.inspect_labbcat(corpus_root)
 parser.call_back = print
 
 with CorpusContext(corpus_name) as c:
-   c.reset()
+    c.reset()
 
-   print('IMPORT...')
-   c.load(parser, corpus_root)
+    print('IMPORT...')
+    c.load(parser, corpus_root)
+
+    print('Speakers:', c.speakers)
+    print('Discourses:', c.discourses)
+
+    # Optional: Use order_by to enforce ordering on the output for easier comparison with the sample output.
+    q = c.query_graph(c.phone).order_by(c.phone.label).group_by(c.phone.label.column_name('phone'))
+    results = q.aggregate(Count().column_name('count'), Average(c.phone.duration).column_name('average_duration'))
+
+    for r in results:
+        print('The phone {} had {} occurrences and an average duration of {}.'.format(r['phone'], r['count'], r['average_duration']))
+
